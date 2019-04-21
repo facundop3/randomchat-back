50636e233a2f198411c13dd9a7768ca31fa2274d
Facundo Petre <facu1p3@gmail.com>
2019-04-21 14:28:15 -0300

    Private messages almost done

--- index.js
+++ index.js
@@ -5,16 +5,33 @@ const http = require('http').createServer(app),
 
 const socketID_username = {}
 
+const getOnlineUsers = id_username => Object.keys(id_username).map(id=> ({username: id_username[id], id}))
+
 io.on('connection', socket =>{
   console.log(`New socket: Id#: ${socket.id}`)
   socket.on('new-message', msg =>{
-    io.emit('new-message', {message:msg, username :socketID_username[socket.id]})
+    console.log(`New socket: Id#: ${socket.id}`)
+    if(socketID_username[socket.id]){
+      io.emit('new-message', {message:msg, username :socketID_username[socket.id]})
+    }
   })
   socket.on('new-username', username =>{
     console.log(`Id: ${socket.id} New user: ${username}`)
     if(!socketID_username[socket.id]){
       socketID_username[socket.id] = username
-    } 
+    }
+    io.emit('all-users', getOnlineUsers(socketID_username))
+  })
+
+  socket.on('new-private-message', ({id, message})=>{
+    console.log(`New socket: Id#: ${socket.id}`)
+    console.log(`Private message to ${id}: ${message}`)
+    io.to(id).emit('new-private-message', {id:socket.id ,message, username:socketID_username[socket.id] })
+  })
+
+  socket.on('disconnect', ()=>{
+    delete socketID_username[socket.id]
+    io.emit('all-users', getOnlineUsers(socketID_username))
   })
 })
 
